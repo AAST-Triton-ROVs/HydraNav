7183934a7583e378aaf94f832ba8b0f3c515d539
Adham Waheeb <adhamwaheeb4444@gmail.com>
2025-02-17 19:28:38 +0200

    gripper daemon/enum

--- src/rov/enums.py
+++ src/rov/enums.py
@@ -48,3 +48,13 @@ class SystemModes(Enum):
     SURFACE = 9
     POSHOLD = 16
     MANUAL = 19
+
+class GripperCommands(Enum):
+    RESET = 0 
+    OPEN = 1
+    CLOSE = -1
+    PITCH_UP = 2
+    PITCH_DOWN = -2
+    ROLL_RIGHT = 3
+    ROLL_LEFT = -3
+    
