b35d1243981520dd204a1071c13b464e8310f22f
jsyph <youlof123@gmail.com>
2025-03-06 12:56:34 +0200

    create docstrings for logger

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -5,7 +5,22 @@ __exports__ = ["logging", "LogLevels"]
 
 
 class Logging:
+    """
+    The Logging class provides a simple interface for storing and managing log messages in a ring buffer.
+    It uses the loguru package to configure both console and file logging.
+
+    :param max_messages: The maximum number of log messages to store in memory.
+    :type max_messages: int
+    :default max_messages: 20
+    """
     def __init__(self, max_messages: int = 20) -> None:
+        """
+        Initialize the logger.
+        
+        :param max_messages: The maximum number of messages to retain.
+        :type max_messages: int
+        """
+        
         self.__max_messages: int = max_messages
         self.__messages: list[str] = [""] * self.__max_messages
 
@@ -18,6 +33,17 @@ class Logging:
         )
 
     def set_level(self, level: LogLevels):
+        """
+        Set the log level for the logger.
+        Removes any existing handlers and adds a new console and file handler with
+        the specified logging level.
+        
+        :param level: An instance of the LogLevels enum specifying the logging level.
+        :type level: LogLevels
+        :returns: None
+        :rtype: None
+        """
+        
         self.logger.remove()
 
         self.logger.add(
@@ -33,9 +59,24 @@ class Logging:
         )
 
     def set_level_str(self, level: str):
+        """
+        Set the logging level using a string value.
+        
+        :param level: The desired logging level name. The value must be a valid key in ``LogLevels``.
+        :type level: str
+        :raises KeyError: If the specified logging level does not exist in ``LogLevels``.
+        """
+        
         self.set_level(LogLevels[level.upper()])
 
     def get_messages(self) -> list[str]:
+        """
+        Return the stored messages, up to the maximum allowed.
+        
+        :returns: A list of the stored messages.
+        :rtype: list[str]
+        """
+        
         return self.__messages[-self.__max_messages :]
 
     def __handler(self, message: str) -> None:
