3ecd02e772e840c6216c7f68ca6035fd3dc92759
jsyph <youlof123@gmail.com>
2025-04-08 01:50:02 +0200

    use a new voice

--- scripts/generate_audio_lines.py
+++ scripts/generate_audio_lines.py
@@ -2,7 +2,7 @@ from dimits import Dimits # type: ignore
 import tqdm # type: ignore
 
 
-dt = Dimits("en_GB-alan-medium")
+dt = Dimits("en_GB-jenny_dioco-medium")
 
 with open("assets/audio/audio_lines") as file:
     lines = file.readlines()
