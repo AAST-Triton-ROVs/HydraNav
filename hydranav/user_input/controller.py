7b8725f18db8f99367c9cd5fd4d1eee7eab0874f
jsyph <youlof123@gmail.com>
2025-04-11 21:52:33 +0200

    process hats now calls input mapper

--- hydranav/user_input/controller.py
+++ hydranav/user_input/controller.py
@@ -260,7 +260,7 @@ class Controller:
 
             controller_button = self.__library_hat_mappings[direction]
 
-            event_dispatcher.dispatch("controller/button_down", controller_button)
+            self.__input_mapper.button_down(controller_button)
             system_logger.info(f"Controller hat pressed: {controller_button}")
 
             self.__previous_hat_value = (int(direction[0]), int(direction[1]))
