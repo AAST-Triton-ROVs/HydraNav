e1da30c6806483f1353d68e7e60f124f323b6099
jsyph <youlof123@gmail.com>
2025-04-12 16:13:14 +0200

    rework keyboard input

--- hydranav/user_input/keyboard.py
+++ hydranav/user_input/keyboard.py
@@ -1,5 +1,6 @@
 import pygame
 from user_input.keyboard_keys import KeyboardKeys
+from user_input.input_mapper import input_mapper
 from core import system_logger, event_dispatcher
 
 __all__ = ["Keyboard", "KeyboardKeys"]
@@ -11,19 +12,13 @@ class Keyboard:
 
     def update(self):
         for button_event in pygame.event.get([pygame.KEYDOWN]):
-            event_dispatcher.dispatch(
-                "keyboard_button_down",
-                KeyboardKeys.from_pygame_key(button_event.key),
-            )
+            processed_key = KeyboardKeys.from_pygame_key(button_event.key)
+            if processed_key is None:
+                system_logger.warning(
+                    f"{button_event.key} is not a supported button"
+                )
+                continue
+            input_mapper.button_down(f"K_{processed_key.name}")
             system_logger.info(
-                f"keyboard button {KeyboardKeys.from_pygame_key(button_event.key).value} pressed down"
-            )
-
-        for button_event in pygame.event.get([pygame.KEYUP]):
-            event_dispatcher.dispatch(
-                "keyboard_button_up",
-                KeyboardKeys.from_pygame_key(button_event.key),
-            )
-            system_logger.info(
-                f"keyboard button {KeyboardKeys.from_pygame_key(button_event.key).value} pressed up"
+                f"keyboard button {KeyboardKeys.from_pygame_key(button_event.key).name} pressed down"
             )
