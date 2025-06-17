8903c7ec6e3dc20857cdc64c5e8f6ae306455232
jsyph <youlof123@gmail.com>
2025-06-17 20:08:20 +0300

    invert gripper jaw closing commands

--- hydranav/manfaloty/enums.py
+++ hydranav/manfaloty/enums.py
@@ -9,10 +9,10 @@ class ManfalotyCommands(Enum):
        Each value is an integer that corresponds to a specific action.
     """
 
-    GRIPPER_JAW_CLOSE = 1
+    GRIPPER_JAW_CLOSE = -1
     """Closes the gripper jaws."""
 
-    GRIPPER_JAW_OPEN = -1
+    GRIPPER_JAW_OPEN = 1
     """Opens the gripper jaws."""
 
     RELAY_ON = 2
