69f7917812db6365afabf22dc8931c9dcdff76e1
jsyph <youlof123@gmail.com>
2025-10-09 22:07:44 +0300

    rework TTS storage system to use the user's data folder

--- hydranav/core/tts.py
+++ hydranav/core/tts.py
@@ -1,17 +1,20 @@
 import os
 from pathlib import Path
+import platformdirs
 from playsound3 import playsound
 from hydranav.core.event_dispatcher import event_dispatcher
-from hydranav.core.config_manager import config_manager
+from hydranav.core.config_manager import config_manager, APP_NAME
 from hydranav.core.logger import LoggerMixin
 from dimits import Dimits  # type: ignore
 from xxhash import xxh3_128_hexdigest
 import multiprocessing
 
 LineID = str
-ASSETS_PATH = config_manager["tts", "assetsPath"]
 TTS_MODEL = config_manager["tts", "piperModel"]
 TTS_LINE_GEN_TIMEOUT = config_manager["tts", "genTimeout"]
+DATA_DIR_PATH = platformdirs.user_data_dir(appname=APP_NAME)
+TTS_RECORDINGS_DIR_PATH = Path(DATA_DIR_PATH, "tts", "recordings")
+MODEL_PATH = Path(DATA_DIR_PATH, "tts")
 
 
 class TextToSpeech(LoggerMixin):
@@ -22,16 +25,21 @@ class TextToSpeech(LoggerMixin):
         self.__lines_to_generate: dict[LineID, str] = {}
         self.__lines: dict[LineID, str] = {}
 
-        try:
-            self.__lines_on_disk = [x.split(".")[0] for x in os.listdir(ASSETS_PATH)]
-        except IOError as e:
-            self._logger.error(f"Failed tp read lines: {e}")
-
         self.__dt = None
         self.__enabled = True
 
+        os.makedirs(TTS_RECORDINGS_DIR_PATH, exist_ok=True)
+        os.makedirs(MODEL_PATH, exist_ok=True)
+
+        try:
+            self.__lines_on_disk = [
+                x.split(".")[0] for x in os.listdir(TTS_RECORDINGS_DIR_PATH)
+            ]
+        except IOError as e:
+            self._logger.error(f"Failed tp read lines: {e}")
+
     def init(self):
-        self.__dt = Dimits(TTS_MODEL)
+        self.__dt = Dimits(TTS_MODEL, modelDirectory=str(MODEL_PATH))
 
     def register_line(self, line: str) -> LineID:
         line_id = xxh3_128_hexdigest(line)
@@ -39,7 +47,7 @@ class TextToSpeech(LoggerMixin):
             self.__lines_to_generate[line_id] = line
             self._logger.info(f"'{line}' is an unregistered line")
         else:
-            self.__lines[line_id] = str(Path(ASSETS_PATH, f"{line_id}.wav"))
+            self.__lines[line_id] = str(Path(TTS_RECORDINGS_DIR_PATH, f"{line_id}.wav"))
             self._logger.debug(f"'{line}' is registered line")
 
         return line_id
@@ -61,7 +69,15 @@ class TextToSpeech(LoggerMixin):
             self._logger.error("TTS is not initialized")
             return
 
-        self.__dt.text_2_audio_file(f"{line}..", f"{line_id}", ASSETS_PATH)
+        try:
+            self.__dt.text_2_audio_file(
+                f"{line}..",
+                f"{line_id}",
+                str(TTS_RECORDINGS_DIR_PATH),
+                format="wav",
+            )
+        except OSError as e:
+            self._logger.error(f"Failed to generate TTS line, with error: {e}")
 
     def update_and_generate_lines(self):
         unexpected_lines = [
@@ -92,14 +108,16 @@ class TextToSpeech(LoggerMixin):
                 self._logger.success(f"Generated '{line}'")
 
             try:
-                self.__lines[line_id] = str(Path(ASSETS_PATH, f"{line_id}.wav"))
+                self.__lines[line_id] = str(
+                    Path(TTS_RECORDINGS_DIR_PATH, f"{line_id}.wav")
+                )
                 self._logger.success(f"Registered '{line}' with '{line_id}'")
             except FileNotFoundError:
                 self._logger.error(f"Failed to generate file for '{line}'.")
-    
+
     def enable(self):
         self.__enabled = True
-    
+
     def disable(self):
         self.__enabled = False
 
